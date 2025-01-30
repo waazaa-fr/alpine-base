6b4adeb728a665e87c260f490c59081f3b505e11
waazaa <waazaa@waazaa.fr>
2025-01-30 17:10:36 +0100

    Correction pour ajouter supervisord

---
+++ root/app/scripts/startup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+##############################################################################################################
+######  Les scripts locaux dans /app/scripts/ sont lancés un après l'autre par ordre alphanumérique
+##############################################################################################################
+find /app/scripts/scripts/ -iname "*.sh" | \
+while read I; do
+    chmod a+x "$I"
+    sh "$I"
+done
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
